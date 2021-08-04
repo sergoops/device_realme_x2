53bcaa2a7fd5819f6ba067bacf2ea20a4a2dda45
Retypolkg <damian11vp.pl@gmail.com>
2021-08-04 20:03:04 +0200

    X2: Update to redfin August fp

--- ssos_X2.mk
+++ ssos_X2.mk
@@ -19,7 +19,7 @@ PRODUCT_MANUFACTURER := realme
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
