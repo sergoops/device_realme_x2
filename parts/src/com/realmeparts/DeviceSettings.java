adithya2306/android_device_realme_x3

x3: parts: Nuke useless touch sampling boost
 * this device supports only constant touch sampling rate
   of 120Hz, so game_switch node is useless and perhaps
   problematic as well

 * bring back edge limit handling while we're at it

parts/src/com/realmeparts/DeviceSettings.java
@@ -48,7 +48,9 @@ public class DeviceSettings extends PreferenceFragment
     public static final String KEY_VIBRATION_STRENGTH = "vibration_strength";
     public static final String CABC_SYSTEM_PROPERTY = "persist.cabc_profile";
     public static final String VIB_STRENGTH_SYSTEM_PROPERTY = "persist.vib_strength";
+    public static final String GAME_MODE_SYSTEM_PROPERTY = "persist.perf_profile";
     public static final String TP_DIRECTION = "/proc/touchpanel/oplus_tp_direction";
+    public static final String TP_LIMIT_ENABLE = "/proc/touchpanel/oplus_tp_limit_enable";
     private static NotificationManager mNotificationManager;
     private Vibrator mVibrator;
     public TwoStatePreference mDNDSwitch;

sergoops/device_realme_x2

parts/src/com/realmeparts/DeviceSettings.java
@@ -48,7 +48,9 @@ public class DeviceSettings extends PreferenceFragment
     public static final String KEY_VIBRATION_STRENGTH = "vibration_strength";
     public static final String CABC_SYSTEM_PROPERTY = "persist.cabc_profile";
     public static final String VIB_STRENGTH_SYSTEM_PROPERTY = "persist.vib_strength";
+    public static final String GAME_MODE_SYSTEM_PROPERTY = "persist.perf_profile";
     public static final String TP_DIRECTION = "/proc/touchpanel/oppo_tp_direction";
+    public static final String TP_LIMIT_ENABLE = "/proc/touchpanel/oppo_tp_limit_enable";
     private static NotificationManager mNotificationManager;
     private Vibrator mVibrator;
     public TwoStatePreference mDNDSwitch;